c156d00c8e13aab6e95c902d716c17fbb720c934
Aleksey6453 <login6453@gmail.com>
2023-09-30 11:32:23 +0300

    first commit for next

---
+++ app/about/page.jsx
@@ -0,0 +1,9 @@
+export default function About(){
+    return <div className="wrapPage">
+        <h1 className="title">About page</h1>
+        <img src="oleta.jpeg" alt="img" className="img" />
+    </div>
+    
+}
+
+
