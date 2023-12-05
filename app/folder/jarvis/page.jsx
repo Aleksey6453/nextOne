e48c5615bb30ad00ee2d1c1a3f2fc22b7d9ef3e5
Aleksey6453 <login6453@gmail.com>
2023-12-05 20:58:31 +0200

    add button for Create new post

--- app/folder/jarvis/page.jsx
+++ app/folder/jarvis/page.jsx
@@ -1,10 +1,27 @@
+import Link from 'next/link';
 import Feed from './Feed'
 
+export const metadata ={
+    title: "Jarvis",
+    descrition: "About Jarvis"
+}
+
 export default function Jarvis(){
+    const isUserLoginIn = true;
+
     return (
         <div className='wrapPage'>
             <h1>Jarvis</h1>
             <Feed />
+            
+            {isUserLoginIn ? (
+                <div>
+                    <Link href='/create-prompt' className='btn'>
+                        Create new post...
+                    </Link>
+                </div>
+            ) : 
+            <h1>User is dont Logining</h1>}
         </div>
     )
 }
