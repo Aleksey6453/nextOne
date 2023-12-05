e49248ba7bc31b008cced36fb505f4a5dd89c841
Aleksey6453 <login6453@gmail.com>
2023-12-05 22:16:33 +0200

    add last changes

--- app/folder/jarvis/page.jsx
+++ app/folder/jarvis/page.jsx
@@ -1,5 +1,11 @@
+'use client'
+
+import React from 'react';
+import { useEffect, useState } from 'react';
+import { sign } from 'crypto';
 import Link from 'next/link';
 import Feed from './Feed'
+import {signIn, signOut, useSession, getProviders} from 'next-auth/react'
 
 export const metadata ={
     title: "Jarvis",
@@ -8,6 +14,20 @@ export const metadata ={
 
 export default function Jarvis(){
     const isUserLoginIn = true;
+    const signOut = () => {
+        
+    }
+
+    const [providers, setProviders] = useState(null)
+    useEffect(() => {
+        const setProviders = async() => {
+            const response = await getProviders()
+
+            setProviders(response)
+        }
+        setProviders()
+    }, [])
+
 
     return (
         <div className='wrapPage'>
@@ -16,12 +36,28 @@ export default function Jarvis(){
             
             {isUserLoginIn ? (
                 <div>
-                    <Link href='/create-prompt' className='btn'>
+                    <Link href='/create-prompt' className='btn m-10'>
                         Create new post...
                     </Link>
+                    <button className='btn m-10' onClick={signOut}>
+                        Sign Out
+                    </button>
                 </div>
-            ) : 
-            <h1>User is dont Logining</h1>}
+            ) : (
+                <>
+                { providers &&
+                   Object.values(providers).map((provider) => (
+                    <button type='button' 
+                            key={provider.name} 
+                            onClick={()=>signIn(provider.id)} 
+                            className='btn m-10'>
+                                Sign In!
+                    </button>
+                   ))}
+                </>
+            )
+
+            }
         </div>
     )
 }
