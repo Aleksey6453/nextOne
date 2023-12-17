655f8c223adadd687d4c380d52375df113b31f9f
Aleksey6453 <login6453@gmail.com>
2023-12-18 00:47:19 +0200

    add last changes

--- app/api/posts/route.js
+++ app/api/posts/route.js
@@ -1,16 +1,31 @@
 import { NextResponse } from "next/server";
+import Post from '../../models/Post'
+import connect from '../../utils/db'
 
-export async function GET(req){
-    return NextResponse.json({message: 'hello!'})
+
+export const GET = async(request) => {
+    try{
+        await connect()
+        const posts = await Post.find()
+
+        return new NextResponse(posts, {status: 200})
+    }catch(error){
+        return new NextResponse('Error in response of db', {status: 500})
+    }
 }
 
-//http://localhost:3000/api/users
-export async function GET(req){
-    const users =  [
-        {id: 1, name: 'One'},
-        {id: 2, name: 'Two'},
-        {id: 3, name: 'Three'},
-    ]
 
-    return new Response(JSON.stringify(users))
-}
+// export async function GET(req){
+//     return NextResponse.json({message: 'hello!'})
+// }
+
+// //http://localhost:3000/api/users
+// export async function GET(req){
+//     const users =  [
+//         {id: 1, name: 'One'},
+//         {id: 2, name: 'Two'},
+//         {id: 3, name: 'Three'},
+//     ]
+
+//     return new Response(JSON.stringify(users))
+// }
